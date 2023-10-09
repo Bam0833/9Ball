17156ef10b8424700fb8a6157d1f703da6e14398
Brandon Morgan <63347492+Bam0833@users.noreply.github.com>
2023-10-09 05:18:59 -0400

    Add files via upload

---
+++ nineBall.py
@@ -0,0 +1,33 @@
+import random
+
+
+def new_nine_ball():
+ def question_one() :
+     question_one() == input("Please input the question to see my answer, or you may type 'E' to quit the game:      ")
+nine_ball_answers =[
+"You want the truth",
+"If I say yes Will you please leave me alone",
+"NO NO NO NO NO",
+"Generating answer, please wait"
+"How should I know?",
+"Yes",
+"Are you sure you want the answer to that?",
+"Id not disagree",
+"Would your mom be ok with this question?",
+"Does it really matter?",
+"Thats not what I heard",
+"Loading",
+"So it shall be",
+"I could say yes, but thats just me guessing",
+"Shame on you for asking such a question",
+"Maybe you should Google it",
+"If the internet says its true then it must be",
+"All out of responses Please try again later",
+"May the odds forever be in your favor",
+"Its whatever you think Im just a magic 9 ball program not a mind reader"
+]
+print(random.choice(nine_ball_answers),"\n")
+new_nine_ball()
+
+new_nine_ball()
+
